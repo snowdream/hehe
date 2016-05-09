9de15e3d1a15c4043b268f9526a10921b0206551
snowdream <yanghui1986527@gmail.com>
2016-05-09 12:08:20 +0800

    Add:  morgan for log
    see:
    https://github.com/expressjs/morgan

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs')
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var FileStreamRotator = require('file-stream-rotator')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -15,18 +17,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (app.get('env') === 'development') {
-  app.locals.pretty = true;
+    app.locals.pretty = true;
+    app.use(logger('dev'));
 
 
-}else{
-  app.locals.pretty = false;
+} else {
+    app.locals.pretty = false;
+
+    //log config for morgan
+    var logDirectory = __dirname + '/log'
+    // ensure log directory exists
+    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+    // create a rotating write stream
+    var accessLogStream = FileStreamRotator.getStream({
+        date_format: 'YYYYMMDD',
+        filename: logDirectory + '/access-%DATE%.log',
+        frequency: 'daily',
+        verbose: false
+    });
+
+    // setup the logger
+    app.use(logger('combined', {stream: accessLogStream}))
+
 
 }
 
@@ -35,10 +53,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -46,23 +64,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
