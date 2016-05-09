94e7d0b94600fb569418d8bc72f278af95087585
snowdream <yanghui1986527@gmail.com>
2016-05-09 17:24:09 +0800

    Add:  accepts for content negotiation
    see:
    https://www.npmjs.com/package/accepts
    https://www.npmjs.com/package/negotiator

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var accepts = require('accepts')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    var accept = accepts(req);
+
+    // the order of this list is significant; should be server preferred order
+    switch (accept.type(['json', 'html'])) {
+        case 'json':
+            res.json({"title": "Express", "content": "Welcome to Express"});
+            break;
+        case 'html':
+            res.render('index', {title: 'Express'});
+            break;
+        default:
+            // the fallback is text/plain, so no need to specify it above
+            res.setHeader('Content-Type', 'text/plain');
+            res.write('hello, world!');
+            res.end();
+            break;
+    }
 });
 
 module.exports = router;
